ac30194ffb86bde0a6e23c0e515b4ef7582fad44
Craig Duncan <git@duncanc.co.uk>
2016-10-02 16:07:47 +0100

    Abstract the bom handling to a separate class

---
+++ src/Handler.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace duncan3dc\Bom;
+
+class Handler
+{
+    const UNKNOWN = "";
+    const UTF8 = "\xEF\xBB\xBF";
+    const UTF16BE = "\xFE\xFF";
+    const UTF16LE = "\xFF\xFE";
+
+    /**
+     * @var string $encoding The encoding that the data we're handling is in.
+     */
+    private $encoding;
+
+
+    /**
+     * Get the encoding of a BOM declared string, and remove the bom.
+     *
+     * @param string $string The data to examine
+     *
+     * @return string The encoding of the string
+     */
+    private function getEncoding(&$string)
+    {
+        # Check for the UTF-8 byte order mark
+        if (substr($string, 0, 3) === self::UTF8) {
+            $string = substr($string, 3);
+            return self::UTF8;
+        }
+
+        # Check for the UTF-16 big endian byte order mark
+        if (substr($string, 0, 2) === self::UTF16BE) {
+            $string = substr($string, 2);
+            return self::UTF16BE;
+        }
+
+        # Check for the UTF-16 little endian byte order mark
+        if (substr($string, 0, 2) === self::UTF16LE) {
+            $string = substr($string, 2);
+            return self::UTF16LE;
+        }
+
+        return self::UNKNOWN;
+    }
+
+
+    /**
+     * Read some data, remove any BOM found, and convert to UTF-8.
+     *
+     * @param string $string The data to read/convert
+     *
+     * @param string
+     */
+    public function convert($string)
+    {
+        if ($this->encoding === null) {
+            $this->encoding = $this->getEncoding($string);
+        }
+
+        if ($this->encoding === self::UTF16BE) {
+            $string = mb_convert_encoding($string, "UTF-8", "UTF-16BE");
+        }
+
+        if ($this->encoding === self::UTF16LE) {
+            $string = mb_convert_encoding($string, "UTF-8", "UTF-16LE");
+        }
+
+        return $string;
+    }
+}
--- src/Util.php
+++ src/Util.php
@@ -4,34 +4,18 @@ namespace duncan3dc\Bom;
 
 class Util
 {
-    const UTF8 = "\xEF\xBB\xBF";
-    const UTF16BE = "\xFE\xFF";
-    const UTF16LE = "\xFF\xFE";
 
+    /**
+     * Remove a BOM from a file contents and convert to UTF-8.
+     *
+     * @param string $string The file contents to convert
+     *
+     * @param string
+     */
     public static function removeBom($string)
     {
-        # Check for the UTF-8 byte order mark
-        if (substr($string, 0, 3) === self::UTF8) {
-            # If we found the bom then the file is already UTF-8 so just remove the bom
-            return substr($string, 3);
-        }
+        $handler = new Handler;
 
-        # Check for the UTF-16 big endian byte order mark
-        if (substr($string, 0, 2) === self::UTF16BE) {
-            # If we found the bom then remove it and convert the data from UTF-16 to UTF-8
-            $string = substr($string, 2);
-            $string = mb_convert_encoding($string, "UTF-8", "UTF-16BE");
-            return $string;
-        }
-
-        # Check for the UTF-16 little endian byte order mark
-        if (substr($string, 0, 2) === self::UTF16LE) {
-            # If we found the bom then remove it and convert the data from UTF-16 to UTF-8
-            $string = substr($string, 2);
-            $string = mb_convert_encoding($string, "UTF-8", "UTF-16LE");
-            return $string;
-        }
-
-        return $string;
+        return $handler->convert($string);
     }
 }
